2fab9c5909cdab68169100357f5264fee165470b
Kevin Kirsch <keine.ahnung.ka@googlemail.com>
2012-02-22 18:56:19 +0100

    ComunicationConstants ins Roboter-Projekt ausgelagert

--- src/de/c3t/BehaviorRoboter/RC/ComunicationConstants.java
+++
@@ -1,8 +0,0 @@
-package de.c3t.BehaviorRoboter.RC;
-
-public class ComunicationConstants {
-	public static final byte exit = 0;
-	public static final byte forward = 1;
-	public static final byte backward = 2;
-
-}
